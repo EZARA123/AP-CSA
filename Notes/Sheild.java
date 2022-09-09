4ba890a000f2d39d658f59743a965ffcbd04e5eb
EZARA123 <ez.zaragoza123@gmail.com>
2022-09-09 11:37:35 -0700

    objects

---
+++ Notes/Sheild.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Phone here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Sheild
+{
+    private String metal;
+    private String shape;
+    private double size;
+    private int durability = 100;
+    
+}
