73c87258f3c068d634d6298ef3f560e7e2568f74
EZARA123 <ez.zaragoza123@gmail.com>
2022-09-12 11:36:10 -0700

    Black jack

--- Notes/Sheild.java
+++ Notes/Sheild.java
@@ -1,4 +1,4 @@
-
+import java.awt.Color;
 /**
  * Write a description of class Phone here.
  *
@@ -11,5 +11,30 @@ public class Sheild
     private String shape;
     private double size;
     private int durability = 100;
+    private Color color;
+    private String handle;
     
+    
+    public String owner;
+    public Sheild(String metalType, String sheildShape, String sheildHandle,
+    double size, Color color)
+    {
+        metal = metalType;
+        shape = sheildShape;
+        handle = sheildHandle;
+        this.size = size;
+        this.color = color;
+    }
+
+    public Sheild(){
+        metal = "Titianium";
+        shape = "Pointed";
+        handle = "Wood";
+        size = 4.3;
+        color = new Color(184, 184, 184);
+    }
+    
+    public String getMetal(){
+        return metal;
+    }
 }
