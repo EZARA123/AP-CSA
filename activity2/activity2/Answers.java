cbcfc3c56b82240502c07ea761eca98497bc821f
EZARA123 <ez.zaragoza123@gmail.com>
2022-09-26 11:26:08 -0700

    done

---
+++ activity2/activity2/Answers.java
@@ -0,0 +1,12 @@
+
+/**
+ * Write a description of class Answers here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Answers
+{
+    //It goes by order by which the code is type it prioritize the order of the responses.
+    //
+}
---
+++ activity3/activity3/answers.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class answers here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class answers
+{
+    /*
+     * findKeyword("She's my sister", "sister", 0);
+     * 
+     * Iteration    psn     before      after
+     * 1             9      " "         "" 
+     * 
+     * findKeyword("Brother Tom is helpful, "brother", 0);
+     * 
+     * Iteration    psn     before      after
+     * 1            0       ""          ""   
+     * 
+     * findKeyword("I can't catcth wild cats.", "cat", 0);
+     * 
+     * Iteration    psn     before      after
+     * 1            8      " "         "c"
+     * 2            19     " "         "s"
+     * 
+     * findKeyword("I know nothing about snow plows.", "no", 0);
+     * 
+     * Iteration    psn     before      after
+     * 1            3       "k"         "w"
+     * 2            7       " "         "t"
+     * 3            22      "s"         "w"
+     */
+}
