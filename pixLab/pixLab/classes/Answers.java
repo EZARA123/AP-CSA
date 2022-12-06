114494b161c295547c7aad267acff6573cb702c9
EZARA123 <ez.zaragoza123@gmail.com>
2022-12-06 10:48:40 -0800

    pixLab

---
+++ pixLab/pixLab/classes/Answers.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class Answers here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Answers
+{
+    /*
+     * Answers:
+     * 
+     * A1:
+     * 1. 8 bits
+     * 2. 3 bytes
+     * 3. 307,200pixels
+     * 
+     * A2:
+     * 1. Set Red at 255, set Green at 0, and set Blue at 255
+     * 2. Set Red at 255, set Green at 255, and set Blue at 0
+     * 3. Set Red at 125, set Green at 0, and set Blue at 255
+     * 4. Set everything at 255
+     * 5. Set every color at 50
+     * 
+     * A3:
+     * 1. The row index is 0
+     * 2. The column index is 0
+     * 3. The right most column index is 639
+     * 4. The bottom most row index is 479
+     * 5. The row increases from top to bottom
+     * 6. The column increase from left to right
+     * 7. Yes you can see squares of color
+     */
+    
+}
